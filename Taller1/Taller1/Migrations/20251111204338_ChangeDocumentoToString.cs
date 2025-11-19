e73e8a13d3664d74c903ed0ce43398b0fe2bc2d9
sebastianm7713@gmail.com <sebastianm7713@gmail.com>
2025-11-19 15:20:06 -0500

    Commit

---
+++ Taller1/Taller1/Migrations/20251111204338_ChangeDocumentoToString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taller1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDocumentoToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+            name: "Documento",
+            table: "Clientes",
+            type: "nvarchar(max)",
+            nullable: false,
+            oldClrType: typeof(int));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Clientes");
+        }
+    }
+}
